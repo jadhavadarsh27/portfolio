d47d28ff25001dd87fe5dacfd90091fca51414be
Adarsh <jadhavadarsh27@gmail.com>
2026-01-25 19:20:37 +0530

    Add Sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ function App() {
   
 
   return
-    <>
-     
-    </>
+    <div>
+     classnName="min-h-screen overflow-x-hidden"> 
+     <Navbar />
+     <main>
+      
+     </main>
+
+    </div>
 }
 
 export default App
